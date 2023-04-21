7a112c0635e1157885055591fc35f0562d39b185
zl-java <54029183+zl200@users.noreply.github.com>
2023-04-21 14:24:48 +0800

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -169,7 +169,7 @@ function _Home() {
         className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
       >
         <div className={styles["sidebar-header"]}>
-          <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+          <div className={styles["sidebar-title"]}>智能小助手</div>
           <div className={styles["sidebar-sub-title"]}>
             Build your own AI assistant.
           </div>
