889d3d03afbe76fd8d2a188a53afde6ec80b0b92
zl-java <54029183+zl200@users.noreply.github.com>
2023-04-21 14:23:02 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "智能小助手",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "智能小助手",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
